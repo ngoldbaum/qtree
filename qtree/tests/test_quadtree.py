b8b4b8b442e0336ea9941728689ad654a4dab763
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-09-01 14:24:29 -0500

    add test

---
+++ qtree/tests/test_quadtree.py
@@ -0,0 +1,20 @@
+import numpy as np
+from qtree import ParticleQuadTreeNode
+
+
+def test_quadtree():
+    np.random.seed(0x4d3d3d3)
+    input_npart = 1000
+    positions = np.random.normal(loc=0.5, scale=0.1, size=(1000, 2))
+    positions = np.clip(positions, 0, 1.0)
+
+    tree = ParticleQuadTreeNode([0.5, 0.5], 0.5)
+
+    tree.insert(positions)
+
+    npart = 0
+
+    for leaf in tree.leaves:
+        npart += leaf.num_particles
+
+    assert npart == input_npart
