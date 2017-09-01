184923164235bd07d7367728a24b7e2b2bd25bfd
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-09-01 13:29:02 -0500

    Added the wrong file

--- qtree/quad_tree.py
+++ qtree/quad_tree.py
@@ -1,3 +1,150 @@
-from yt.units import cm, m
+import enum
+
 import numpy as np
-print(np.add([1, 1, 1]*cm, [1, 1, 1]*m))
+from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
+
+
+@enum.unique
+class _Direction(enum.Enum):
+    SOUTHWEST = (0, 0)
+    SOUTHEAST = (1, 0)
+    NORTHWEST = (0, 1)
+    NORTHEAST = (1, 1)
+
+
+_NODE_CAPACITY = 4
+
+_offsets = {
+    _Direction.SOUTHWEST: np.array((-1, -1)),
+    _Direction.SOUTHEAST: np.array((1, -1)),
+    _Direction.NORTHWEST: np.array((-1, 1)),
+    _Direction.NORTHEAST: np.array((1, 1)),
+}
+
+
+class ParticleQuadTreeNode(object):
+
+    def __init__(self, center, half_width):
+        """A QuadTree data structure containing particles
+
+        Parameters
+        ----------
+        center : 2-element iterable
+            The center of the quad tree node
+        half_width : float
+            The half-width of the node. Currently only square nodes are
+            supported.
+        """
+        self.positions = np.empty((_NODE_CAPACITY, 2))
+        self.positions[:] = np.nan
+        self.num_particles = 0
+
+        center = np.array(center)
+
+        if center.shape != (2,):
+            raise RuntimeError(
+                "Received center with shape %s but expected (2,)"
+                % (center.shape,))
+
+        self.left_edge = center - half_width
+        self.right_edge = center + half_width
+        self.center = center
+        self.half_width = half_width
+
+        self.northeast = None
+        self.northwest = None
+        self.southeast = None
+        self.southwest = None
+
+    def insert(self, positions):
+        """Insert particle into the quadtree
+
+        Parameters
+        ----------
+        position : 2 element iterable or iterable of 2-element iterables
+            Position of the particle to be inserted.
+        """
+        positions = np.asarray(positions)
+        if len(positions.shape) == 1:
+            positions = np.asarray([positions])
+
+        nparticles = positions.shape[0]
+
+        if positions.shape[-1] != 2:
+            raise RuntimeError(
+                "Received %sD positions %s but expected 2D positions"
+                % (positions.shape,))
+
+        # check if particle is inside this node
+        if not ((positions > self.left_edge).all() and
+                (positions < self.right_edge).all()):
+            raise RuntimeError(
+                "positions outside node with left_edge=%s and right_edge=%s"
+                % (self.left_edge, self.right_edge))
+
+        self.num_particles += nparticles
+
+        if self.num_particles <= _NODE_CAPACITY:
+            cur_np = self.num_particles - nparticles
+            self.positions[cur_np: cur_np + nparticles] = positions
+            return
+
+        self.positions = None
+
+        # adding another particle requires refining the tree
+        center = self.center
+        for position in positions:
+            pos_gt_cen = tuple((position > center).astype(int))
+            self._insert_child(pos_gt_cen, position)
+
+    def _insert_child(self, direction, position):
+        child = _Direction(direction)
+        child_name = child.name.lower()
+        child_node = getattr(self, child_name)
+        if child_node is None:
+            offset = _offsets[child]
+            child_node = ParticleQuadTreeNode(
+                self.center + self.half_width / 2 * offset, self.half_width/2)
+            setattr(self, child_name, child_node)
+        child_node.insert(position)
+
+    @property
+    def children(self):
+        for d in _Direction:
+            yield getattr(self, d.name.lower())
+
+    def plot(self, fig=None, axes=None):
+        """Plot this quadtree node and its subtree"""
+        if fig is None and axes is None:
+            fig = plt.figure(figsize=(4, 4))
+            axes = fig.add_axes([.01, .01, .98, .98])
+            axes.set_aspect('equal')
+            plt.axis('off')
+            plt.xlim((0, 1))
+            plt.ylim((0, 1))
+
+        patch = Rectangle(self.left_edge, self.half_width*2, self.half_width*2,
+                          fill=False)
+        axes.add_patch(patch)
+
+        if self.positions is not None:
+            positions = self.positions[:self.num_particles]
+            axes.scatter(positions[:, 0], positions[:, 1], 1, color='k')
+
+        for child in self.children:
+            if child is None:
+                continue
+            child.plot(fig, axes)
+
+
+if __name__ == "__main__":
+    np.random.seed(0x4d3d3d3)
+    positions = np.random.normal(loc=0.5, scale=0.1, size=(1000, 2))
+    positions = np.clip(positions, 0, 1.0)
+
+    tree = ParticleQuadTreeNode([0.5, 0.5], 0.5)
+
+    tree.insert(positions)
+    tree.plot()
+    plt.show()
