0c38cfffe46bba1b437a2e6afdaa652d6810e8b0
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-09-05 13:35:05 -0500

    small refactor of tests

--- qtree/tests/test_quadtree.py
+++ qtree/tests/test_quadtree.py
@@ -5,7 +5,7 @@ from qtree import ParticleQuadTreeNode
 def test_quadtree():
     np.random.seed(0x4d3d3d3)
     input_npart = 1000
-    positions = np.random.normal(loc=0.5, scale=0.1, size=(1000, 2))
+    positions = np.random.normal(loc=0.5, scale=0.1, size=(input_npart, 2))
     positions = np.clip(positions, 0, 1.0)
 
     tree = ParticleQuadTreeNode([0.5, 0.5], 0.5)
